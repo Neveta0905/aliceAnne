bc25b9784cc3d38c620aaa4359ff3242fb10005a
Neveta0905 <lulued@free.fr>
2025-07-14 11:01:27 +0200

    slider done !!

--- scripts/caroussel.js
+++ scripts/caroussel.js
@@ -1,8 +1,18 @@
- let currentSlide = 0;
-const slides = document.getElementById('carousel');
-const totalSlides = slides.children.length;
+let currentSlide = 0;
+let gap= 0
+let currentSlidValue = 0
+const caroussel = document.getElementById('caroussel') || document.getElementsByClassName('caroussel__images')[0];
+const slideList = document.getElementsByClassName("slide")
+const carousselGap = slideList[1].getBoundingClientRect().left - slideList[0].getBoundingClientRect().right
 
 function moveSlide(direction) {
-    currentSlide = (currentSlide + direction + totalSlides) % totalSlides;
-    slides.style.transform = `translateX(-${currentSlide * 100}%)`;
+    let nextSLide = currentSlide + direction === slideList.length ? 0 : currentSlide + direction < 0 ? slideList.length-1 : currentSlide + direction
+    gap = [...slideList].reduce((sum,curr,index)=>{
+        const elemWidth = curr.getBoundingClientRect().width
+        if(index<nextSLide) return sum + elemWidth + carousselGap
+        return sum
+    },0)
+    
+    caroussel.style.transform = `translateX(-${gap}px)`;
+    currentSlide=nextSLide
 }
